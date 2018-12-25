85a60ee36ba1944dcb686c628896e6f37a13cc33
joao <joao182cx@gmail.com>
2018-12-25 21:31:41 -0200

    Criação do método uniao(), que une dois ConjuntosInteiros.

--- src/ConjuntoInteiros.java
+++ src/ConjuntoInteiros.java
@@ -20,5 +20,24 @@ public class ConjuntoInteiros {
 	}
 	
 	
+	public static ConjuntoInteiros uniao(ConjuntoInteiros inteiros1, ConjuntoInteiros inteiros2) {
+		
+		ConjuntoInteiros uniao = new ConjuntoInteiros();
+		
+		for(int i = 0; i < uniao.getInteiros().length; i++) {
+			
+			if(inteiros1.getInteiros()[i] == true || inteiros2.getInteiros()[i] == true) {
+				
+				uniao.getInteiros()[i] = true;
+				
+			}
+			
+		}
+		
+		return uniao;
+		
+	}
+	
+	
 
 }
