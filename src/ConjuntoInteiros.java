5930dcb364b7e4f5f79aa047b7222b34dc23bda2
joao <joao182cx@gmail.com>
2018-12-25 20:40:58 -0200

    Criação das classe ConjuntoInteiros.

---
+++ src/ConjuntoInteiros.java
@@ -0,0 +1,24 @@
+
+public class ConjuntoInteiros {
+	
+	private boolean[] inteiros = new boolean[101];
+	
+	public ConjuntoInteiros() {
+		
+	}
+	
+	public ConjuntoInteiros(boolean[] inteiros) {
+		this.inteiros = inteiros;
+	}
+
+	public boolean[] getInteiros() {
+		return inteiros;
+	}
+
+	public void setInteiros(boolean[] inteiros) {
+		this.inteiros = inteiros;
+	}
+	
+	
+
+}
